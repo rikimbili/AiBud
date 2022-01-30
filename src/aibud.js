e747ae3bd9d45cd90102540b26b57ba07a991207
Raciel <raciel2901@gmail.com>
2022-01-30 05:01:46 -0500

    feat: refactor, add separate json and add reset and set functionalities

--- src/aibud.js
+++ src/aibud.js
@@ -15,8 +15,52 @@ const openai = new OpenAI(process.env.OPENAI_API_KEY); // Initialize OpenAI
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 }); // Initialize the discord client with the right permissions
-let prompts = new Array(); // Initialize the prompts array
-let lastBotMessages;
+let prompts = promptsPreset; // Initialize the prompts array
+let selectedPrompt = "normal"; // Prompt to use
+let lastBotMessages = new Array(process.env.MAX_BOT_PROMPT_HISTORY); // Stores the last bot messages
+let defaultNameNeedsChange = true; // Keeps track of whether the default name needs to be changed
+
+/**
+ * @description Gets the specified prompt
+ *
+ * @param {string} selectedPrompt Prompt to get
+ *
+ * @returns {string} Prompt
+ */
+function getPrompt(selectedPrompt) {
+  for (const [promptKey, promptValue] of Object.entries(prompts)) {
+    if (promptKey === selectedPrompt) {
+      return promptValue;
+    }
+  }
+}
+
+/**
+ * @description Changes the default name throughout the prompts to the user's name
+ *
+ * @param {string} username Name of the user to change the prompt to
+ */
+function changeNameOccurrences(username) {
+  if (defaultNameNeedsChange) {
+    for (const [promptKey, promptValue] of Object.entries(prompts)) {
+      prompts[promptKey] = promptValue.replaceAll("You:", `${username}:`);
+    }
+    defaultNameNeedsChange = false;
+  }
+}
+
+/**
+ * @description Concatenates the specified prompt
+ *
+ * @param {string} newPrompt New prompt to concatenate
+ */
+function concatPrompt(newPrompt) {
+  for (const [promptKey, promptValue] of Object.entries(prompts)) {
+    if (promptKey === selectedPrompt) {
+      prompts[selectedPrompt] = promptValue + newPrompt;
+    }
+  }
+}
 
 /**
  * @description Resets the prompt to default for the user
@@ -24,78 +68,96 @@ let lastBotMessages;
  * @param {Message} message Message object from Discord
  */
 function resetPromptStep(message) {
-  prompts = new Array();
-  message.channel.send(
-    "Reset conversation history for user " + message.author.username
-  );
+  prompts = promptsPreset;
+  message.channel.send("🪄`Memory Reset`🪄");
 }
 
-// /**
-//  * @description Create new prompt for the user
-//  *
-//  * @param {string} userId User ID
-//  * @param {string} prompt Prompt to add
-//  */
-// function createPrompt(userId, prompt) {
-//   if(prompts?.forEach(prompt => prompt.userId === userId)) {
+/**
+ * @description Resets the prompt to default for the user
+ *
+ * @param {Message} message Message object from Discord
+ */
+async function generatePromptStep(message) {
+  const userPrompt = `${message.author.username}: ${message.content
+    .replace("!ai", "")
+    .trim()}\n`;
 
-//   }
-//   prompts.push({
-//     userId: userId,
-//     prompt: prompt,
-//   });
-// }
+  // Add the user's message to the selected prompt
+  concatPrompt(userPrompt);
 
-// /**
-//  * @description Gets the specified prompt for the user
-//  *
-//  * @param {string} userId User ID
-//  * @param {string} selectedPrompt Prompt to get
-//  */
-// function getPrompt(userId, selectedPrompt) {}
+  // Send the prompt to OpenAI and wait for the magic to happen 🪄
+  const gptResponse = await openai.complete({
+    engine: "davinci",
+    prompt: getPrompt(selectedPrompt),
+    maxTokens: 150,
+    temperature: 0.3,
+    topP: 0.3,
+    presencePenalty: 0,
+    frequencyPenalty: 0.5,
+    bestOf: 1,
+    n: 1,
+    stream: false,
+    stop: ["\n", "\n\n"],
+  });
+  const response = gptResponse.data.choices[0]?.text.substring(6).trim();
+
+  // Reset prompt if messages keep repeating
+  for (const message of lastBotMessages) {
+    if (message === response) {
+      resetPromptStep(message);
+      return;
+    }
+  }
+
+  message.reply(`${response}`);
+  concatPrompt(`${gptResponse.data.choices[0].text}\n`);
+}
+
+/**
+ * @description Sets the prompt to the entered prompt
+ *
+ * @param {Message} message Message object from Discord
+ */
+function setEnteredPromptStep(message) {
+  const enteredPrompt = message.content.replace("!ai.set", "").trim();
+
+  if (enteredPrompt.length === 0)
+    return message.reply("`Empty prompt entered\nEnter a valid prompt`");
+
+  // Check if the entered prompt exists and set it to the selected prompt if it does
+  for (const [promptKey, promptValue] of Object.entries(prompts)) {
+    if (promptKey === enteredPrompt) {
+      if (selectedPrompt == enteredPrompt)
+        message.reply("`Behavior prompt already set`");
+      else {
+        selectedPrompt = enteredPrompt;
+        message.reply(`\`Behavior prompt set to ${selectedPrompt}\``);
+      }
+      return;
+    }
+  }
+  message.reply(`\`Behavior prompt ${enteredPrompt} not found\``);
+}
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return; // Return if the message was sent by a bot including AiBud itself
 
-  // Reset the conversation history for the user
+  if (message.content.startsWith("!ai")) {
+    message.channel.sendTyping(); // Show the bot as typing in the channel
+    changeNameOccurrences(message.author.username); // Change the default prompt name occurrences
+  }
+
+  // Reset prompt history case
   if (message.content.startsWith("!ai.reset")) {
-    prompts[message.author.id] = [];
-    message.channel.send(
-      "Reset conversation history for user " + message.author.username
-    );
+    resetPromptStep(message);
+  }
+  // Set entered prompt case
+  else if (message.content.startsWith("!ai.set")) {
+    setEnteredPromptStep(message);
   }
   // Prompt command case
-  else if (message.content.includes("!ai")) {
-    // Make sure the bot appears as typing while the request is processing
-    message.channel.sendTyping();
-
-    // Add the user's message to the personality prompt
-    prompts[message.author.id].baseline += `${
-      message.author.username
-    }: ${message.content.replace("!ai", "").trim()}\n`;
-
-    // Send the prompt to OpenAI and wait for the magic to happen 🪄
-    async () => {
-      const gptResponse = await openai.complete({
-        engine: "davinci",
-        prompt: personality_prompts.baseline,
-        maxTokens: 150,
-        temperature: 0.3,
-        topP: 0.3,
-        presencePenalty: 0,
-        frequencyPenalty: 0.5,
-        bestOf: 1,
-        n: 1,
-        stream: false,
-        stop: ["\n", "\n\n"],
-      });
-      const response = gptResponse.data.choices[0]?.text.substring(7);
-
-      message.reply(`${response}`);
-      personality_prompts.baseline += `${gptResponse.data.choices[0].text}\n`;
-
-      console.log(personality_prompts.baseline);
-    };
+  else if (message.content.startsWith("!ai")) {
+    generatePromptStep(message);
   }
 });
 
